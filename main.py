91c76b65ed40d9d7ecb555911398a2b9a0945791
olenese <74682960+olenese@users.noreply.github.com>
2022-04-20 11:43:34 +0200

    tulling

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 def mainpart(test):
     print(test)
 
+<<<<<<< HEAD
+print("tullebukk")
 print("Testing")
+if __name__ == "__main__":
+    mainpart(test="Hello world")
+=======
 
-if "__name__" == "__main__":
-    mainpart("hello world")
+
+>>>>>>> a05c557d7eb68660d88629c69e6e8d6dac098e93
