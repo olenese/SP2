949174211c211893fc0c64502066e87f98dae18d
olenese <74682960+olenese@users.noreply.github.com>
2022-04-20 11:44:14 +0200

    fixed issues

--- main.py
+++ main.py
@@ -2,12 +2,9 @@
 def mainpart(test):
     print(test)
 
-<<<<<<< HEAD
 print("tullebukk")
 print("Testing")
 if __name__ == "__main__":
     mainpart(test="Hello world")
-=======
 
 
->>>>>>> a05c557d7eb68660d88629c69e6e8d6dac098e93
